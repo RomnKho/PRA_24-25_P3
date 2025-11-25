4903737d85e72dcecd04f2a09c6a4c1737c89d54
Román Khomenko Khomenko <rkhokho@upv.edu.es>
2025-11-25 17:38:34 +0000

    Añadida clase TableEntry y su test

---
+++ TableEntry.h
@@ -0,0 +1,46 @@
+#ifndef TABLEENTRY_H
+#define TABLEENTRY
+
+#include <string>
+#include <ostream>
+
+template <typename V>
+class TableEntry {
+	public:
+		std::string key;
+		V value;
+	
+	public:
+		// Constructor con todos los parámetros
+		TableEntry(std::string key, V value) {
+			this->key = key;
+			this->value = value;
+		}
+
+		// Constructor sin valor
+		TableEntry(std::string key) {
+			this->key = key;
+			this->value = NULL;
+		}
+
+		// Constructor sin parámetros
+		TableEntry() {
+			this->key = "";
+			this->value = NULL;
+		}
+
+		friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+			return (te1.key == te2.key);
+		}
+
+		friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+			return (te1.key != te2.key);
+		}
+
+		friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te) {
+			out << te.key << " -> " << te.value << std::endl;
+			return out;
+		}
+};
+
+#endif
